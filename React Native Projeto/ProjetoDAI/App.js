5ce222416970e7e54ac45d6000a5d3365c8ece48
grupoDAI20202021 <grupoDAI20202021@gmail.com>
2021-03-12 12:51:04 +0000

    asdasd

---
+++ React Native Projeto/ProjetoDAI/App.js
@@ -0,0 +1,71 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { useFonts, RedHatDisplay_400Regular } from '@expo-google-fonts/red-hat-display';
+import AppLoading from 'expo-app-loading';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { StyleSheet, Text, View, TouchableOpacity, TouchableHighlight  } from 'react-native';
+import { faArrowCircleRight } from '@fortawesome/free-solid-svg-icons'
+
+export default function App() {
+  let [fontsLoaded] = useFonts({
+    RedHatDisplay_400Regular,
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        
+        <TouchableHighlight onPress={loginWithAccount} underlayColor={"rgba(15, 122, 190, 0.8)"} style={styles.buttonLogin}>
+          <View style={styles.buttonLoginWithAccountView}>
+            <Text style={styles.textButtonLoginWithAccount}>Entrar com uma conta </Text>
+            <FontAwesomeIcon icon={faArrowCircleRight} style={styles.arrowIconButtonLoginWithAccount} size={30}/>
+          </View>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
+const loginWithAccount = () =>{
+  console.log("Clicaste no botão")
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  buttonLogin:{
+    alignItems: 'center',
+    flexDirection: "row",
+    backgroundColor: 'rgba(26, 130, 196, 0.8)',
+    maxWidth: 370,
+    width:"89%",
+    height: "9%",
+    borderRadius: 50,
+    position:"absolute",
+    bottom:"3%"
+  },
+
+  textButtonLoginWithAccount:{
+    color: "white",
+    fontSize: 22,
+    fontFamily:'RedHatDisplay_400Regular',
+    marginLeft:"15%",
+    width:275
+  },
+
+  arrowIconButtonLoginWithAccount:{
+    color: "white",
+    marginLeft:"-15%",
+  },
+
+  buttonLoginWithAccountView:{
+    flexDirection: 'row', 
+    alignItems: 'center', 
+    justifyContent:'center'
+  }
+});
