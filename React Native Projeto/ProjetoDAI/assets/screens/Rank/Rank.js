8e784d5e88aaf77d4564f06c2ebf141b3b900d2e
grupoDAI20202021 <grupoDAI20202021@gmail.com>
2021-04-09 01:33:42 +0100

    Update Rank.js

--- React Native Projeto/ProjetoDAI/assets/screens/Rank/Rank.js
+++ React Native Projeto/ProjetoDAI/assets/screens/Rank/Rank.js
@@ -1,45 +1,39 @@
-import React, {useState} from 'react';
-import { Text, View, StyleSheet, Animated, ScrollView } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { Text, View, StyleSheet, Animated, ScrollView, Image, FlatList } from 'react-native';
 import AppLoading from 'expo-app-loading';
 import { useFonts, RedHatDisplay_400Regular } from '@expo-google-fonts/red-hat-display';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import { faChevronLeft, faTrophy } from '@fortawesome/free-solid-svg-icons';
 
 // Verificar se o scroll ta bem
 export default function Rank() {
     const scrollY= new Animated.Value(0);
     let scrollYValue = scrollY._value;
     const [scrolled, setScrolled] = useState(false);
+    let rankingPosition = 1;
 
-        /*.then(function (result) {
-            if(result.status.response === "success"){
-                that.setState({ 
-                    JSONResult: result.data_list,
-                });
-            }
-        }*/
-    /*const submit = () => {
-        return fetch(url,{
-            method: 'POST',//GET and ...
-            headers:{
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                elm: "elm" //is fake
-            })
-           })
-           .then(function (response) {
-            return response.json();
-           })
-           .then(function (result) {
-            if(result.status.response === "success"){
-                that.setState({ 
-                    JSONResult: result.data_list,
-                });
-            }
+    const [loaded, setLoaded] = useState(false);
+    
+    //Fetch
+    /*useEffect(() => {
+        async function submit() {
+            const response = await fetch("http://192.168.1.74:8080/api/preferences/7", {//MUDAR O 7
+                method: 'GET',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+            });
+            const response_1 = await response.json();
+            setfavoriteList(response_1);
+            console.log(favoriteList);
+            setLoaded(true);
         }
-        }*/
+        if(!loaded){
+            submit();
+            setLoaded(true);
+        }
+    });*/
 
     let [fontsLoaded] = useFonts({
         RedHatDisplay_400Regular,
@@ -54,15 +48,33 @@ export default function Rank() {
                     <Text style={styles.rankText}>Rank</Text>
                 </View>
                 <View style={styles.rankScreenScrollContainer}>
-                    <Animated.ScrollView style={styles.rankScreenScroll} scrollEventThrottle={1} onScroll={(e)=>{scrollY.setValue(e.nativeEvent.contentOffset.y); scrollYValue = scrollY._value; scrollYValue > 0 ? setScrolled(true) : setScrolled(false);}}>
-                        
-                    </Animated.ScrollView>
+                    {/*<FlatList data={favoriteList} 
+                        renderItem={({ item }) => {
+                            return(
+                                <View style={styles.everyRankView}>
+                                    <View style={styles.myRankPosition}>
+                                        <Text style={styles.positionRank}>#{rankingPosition}</Text>
+                                        <Image source={require("../../avatar1.png")} style={styles.avatar}/>
+                                        <View style={styles.textPositionRank}>
+                                            <Text style={styles.nameTextPositionRank}>adasda</Text>
+                                            <Text style={styles.pointsTextPositionRank}>adasda</Text>
+                                        </View>
+                                        <FontAwesomeIcon icon={faTrophy} style={styles.faTrophy1stPlace} size={25}/>
+                                    </View>
+                                </View>
+                            )
+                        }}
+                        keyExtractor={(item, index) => index}
+                        style={styles.rankScreenScroll}
+                    />*/}
                 </View>
             </View>
         )
     }
 }
-
+const changeRankPosition = () =>{
+    rankingPosition=rankingPosition+1;
+}
 
 const styles = StyleSheet.create({
     container: {
@@ -123,5 +135,53 @@ const styles = StyleSheet.create({
     rankScreenScroll:{
         flex: 5,
         width:"100%",
+    },
+
+    everyRankView:{
+        flex: 1,
+        backgroundColor:"#FCFCFC",
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    myRankPosition:{
+        maxWidth:400,
+        width:"90%",
+        height:80,
+        backgroundColor:"#F0F0F0",
+        borderRadius:50,
+        marginTop:10,
+        alignItems:"center",
+        justifyContent:"space-evenly",
+        flexDirection:"row",
+    },
+
+    textPositionRank:{
+        alignItems:"flex-start",
+        justifyContent:"space-evenly",
+        flexDirection:"column",
+        width:150,
+    },
+
+    avatar:{
+        width:60,
+        height:60,
+    },
+
+    positionRank:{
+        color:"#1A82C4",
+    },
+
+    faTrophy1stPlace:{
+        color:"#DEBB02",
+    },
+
+    nameTextPositionRank:{
+        fontWeight:"bold",
+        color:"#1A82C4",
+    },
+
+    pointsTextPositionRank:{
+        color:"#1A82C4",
     },
 });
